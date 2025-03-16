ff5fdc3199812df9c88d3bae094ade44e2ad82dd
punchwilderness <167172486+punchwilderness@users.noreply.github.com>
2025-03-16 15:25:25 +0530

    Add files via upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-undef */
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://sudqvsecthappldqcjoq.supabase.co";
+export const supabaseUrl = "Supabase URL";
 
-const supabase = createClient(
-  supabaseUrl,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1ZHF2c2VjdGhhcHBsZHFjam9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5OTUyNzksImV4cCI6MjA1NjU3MTI3OX0.YrJGeGm35OiIyWR8iw2nbGkz3UF2IRbGafJjOsF0Rok"
-);
+const supabase = createClient(supabaseUrl, "API KEY");
 
 export default supabase;
